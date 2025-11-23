197a70824d25aecfd6aa914fe2f742bef568dee4
dush <dushan1855@gmail.com>
2025-11-24 00:18:21 +0530

    login and registration completed

--- AuthECAPI/Program.cs
+++ AuthECAPI/Program.cs
@@ -1,7 +1,12 @@
 using AuthECAPI.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -32,6 +37,24 @@ builder.Services.Configure<IdentityOptions>(options =>
 builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DevDB")));
 
+//addAuthentication use for Identity
+builder.Services.AddAuthentication(x => 
+{
+    //A scheme is a way of identifying the authentication handler to be used.
+    x.DefaultAuthenticateScheme =
+    x.DefaultChallengeScheme =
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(y => 
+{
+    y.SaveToken = false;
+    y.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey (
+            Encoding.UTF8.GetBytes(
+                builder.Configuration["AppSettings:JWTSecret"]!))
+    };
+});
 
 var app = builder.Build();
 
@@ -42,6 +65,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+//CORS use for allow request from angular app
+
 #region Config. CORS
 app.UseCors(options =>
 options.WithOrigins("http://localhost:4200")
@@ -49,6 +74,7 @@ options.WithOrigins("http://localhost:4200")
 .AllowAnyHeader());
 #endregion
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
@@ -79,6 +105,36 @@ app.MapPost("/api/signup", async (
             return Results.BadRequest(reuslt);
     });
 
+app.MapPost("/api/signin", async (
+    UserManager<AppUser> userManager,
+    [FromBody] LoginModel loginModel) =>{
+        var user = await userManager.FindByEmailAsync(loginModel.Email);
+        if (user != null && await userManager.CheckPasswordAsync(user, loginModel.Password)) 
+        {
+            var signInKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(
+                builder.Configuration["AppSettings:JWTSecret"]!)
+            );
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim("UserId",user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(10),
+                SigningCredentials = new SigningCredentials(
+                    signInKey,
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+            var token = tokenHandler.WriteToken(securityToken);
+            return Results.Ok(new {token});
+
+        } else
+            return Results.BadRequest(new { message = "Email or Password is incorrect" });
+    });
+
 app.Run();
 
 
@@ -88,4 +144,11 @@ public class UserRegistrationModel
     public string Email { get; set; }
     public string Password { get; set; }
     public string FullName { get; set; }
+}
+
+public class LoginModel
+{
+    //public string UserName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
 }
